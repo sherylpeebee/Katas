63ba2b6aa608dd4da6e3e588ce62ee0979346fbb
Sheryl Boughton <sherpbo@gmail.com>
2015-10-31 20:03:59 -0700

    get rid of superfluous second function

--- framedString.js
+++ framedString.js
@@ -1,20 +1,17 @@
-// Write a function that takes a list of strings an prints them, one per line, in a
-// rectangular frame. For example the list ["Hello", "World", "in", "a", "frame"]
-// gets printed as:
-//
-// *********
-// * Hello *
-// * World *
-// * in    *
-// * a     *
-// * frame *
-// *********
-
-function printWords(str, borderWidth){
+function framedString(str){
   var splitStr = str.split(" "),
-  container = [],
+  framePadding = 4;
+  var sortedByLength = splitStr.sort(function(a, b){
+    return a.length - b.length;
+  });
+  var longest = sortedByLength.pop();
+  var borderWidth = longest.length + framePadding;
+  var frame = Array(borderWidth + 1).join("*") + "\n";
+
+  var splitStr2 = str.split(" ");
+  var container = [],
   newLineBeginning = borderWidth+1;
-  splitStr.forEach(function(word){
+  splitStr2.forEach(function(word){
     for(var i = 0; i <= newLineBeginning; i ++){
       if(i === 0 || i === borderWidth){
         container.push("*");
@@ -30,22 +27,8 @@ function printWords(str, borderWidth){
       }
     }
   });
-  for(var i = 0; i < borderWidth; i++){
-    container.push("*");
-  }
-  return container.join("");
-}
-
-function framedString(str){
-  var splitStr = str.split(" "),
-  framePadding = 4;
-  var sortedByLength = splitStr.sort(function(a, b){
-    return a.length - b.length;
-  });
-  var longest = sortedByLength.pop();
-  var borderWidth = longest.length + framePadding;
-  var frame = Array(borderWidth + 1).join("*") + "\n";
-  console.log(frame + printWords(str, borderWidth));
+  var contents = container.join("");
+  console.log(frame + contents + frame);
 }
 
 framedString("Hello world in a frame");
